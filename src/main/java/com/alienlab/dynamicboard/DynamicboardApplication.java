083fd39cdce2e8631b67d6abac92c77b4445de7d
xubo <xubo123!>
2017-02-19 17:37:57 +0800

    a

---
+++ src/main/java/com/alienlab/dynamicboard/DynamicboardApplication.java
@@ -0,0 +1,12 @@
+package com.alienlab.dynamicboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicboardApplication.class, args);
+	}
+}
