ff5e634bcaef54e02ef244d717940932936eb4e9
juhuiguang <juhuiguang@foxmail.com>
2017-03-07 18:38:07 +0800

    创建前端界面目录

--- src/main/java/com/alienlab/dynamicboard/DynamicboardApplication.java
+++ src/main/java/com/alienlab/dynamicboard/DynamicboardApplication.java
@@ -2,10 +2,16 @@ package com.alienlab.dynamicboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class DynamicboardApplication {
-
+public class DynamicboardApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DynamicboardApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DynamicboardApplication.class, args);
 	}
